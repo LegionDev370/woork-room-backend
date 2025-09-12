407fdd8059252de1de5167254f9ad3f2b61e6a32
LegionDev370 <llegion630@gmail.com>
2025-09-12 15:01:56 +0500

    some code adde

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { OtpService } from './modules/auth/otp.service';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '';
+    return 'Hello World';
   }
 }
